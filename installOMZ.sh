905fcb89d3a1cacff12a856b56da894f6ecdf805
jspaezp <jspaezp@gmail.com>
2017-05-06 19:20:58 -0400

    i dont really know ..

--- installOMZ.sh
+++ installOMZ.sh
@@ -4,3 +4,6 @@
 
 sudo pacman -S --noconfirm --needed zsh zsh-completions zsh-syntax-highlighting
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# ... i know not exactly zsh but it makes sense to have it here
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
